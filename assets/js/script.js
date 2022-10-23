61943754bd3c5d8df091ff0b11a07abb0ca0104c
Zachary Berkley <zachary.berkley@gmail.com>
2022-10-22 22:45:34 -0400

    Added Weather API

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,21 +5,41 @@ var apiKey = '44b7a2530fc38f6834d04bd0e3d9e4b6'
 $('#searchBtn').on('click', function() {
     // stores the user search query in the variable 'cityName'
     var cityName = $('#searchBox').val();
-    // concatenates the API call, the cityName variable, and the apiKey variable in the apiGeocode variable
+    // concatenates the API call, the cityName variable, and the apiKey variable into the apiGeocode variable
     var apiGeocode = 'https://api.openweathermap.org/geo/1.0/direct?q=' + cityName + '&limit=1&appid=' + apiKey;
     // fetch request for the lat and lon of the user's requested city
-    fetch(apiGeocode).then(function(res) {
-        // console.log($(this).get();
-        console.log(res.json());
-    
+    fetch(apiGeocode)
+        .then(function(response) {
+            if (response.ok) {
+                console.log(response);
+                response.json()
+        
+        // logs relevant data to console
+        .then(function (data) {
+            console.log(data);
+            console.log(data[0].lat)
+            console.log(data[0].lon)
 
+            // concatenates the API call, the lon and lat variables, and the apiKey variable into the apiWeather variable
+            var lat = data[0].lat;
+            var lon = data[0].lon;
+            var apiWeather = 'https://api.openweathermap.org/data/2.5/forecast?lat=' + lat + '&lon=' + lon + '&appid=' + apiKey;
+        
+            console.log(apiWeather);
 
-    }) .then(function(data) {
-        console.log(data);
+            return apiWeather;
 
+        })  
 
-    }) 
+        // .catch()
+        // handle the error
+
+    }
 })
+}) 
+
+
+
 
 // test
 // https://api.openweathermap.org/geo/1.0/direct?q=london&limit=1&appid=44b7a2530fc38f6834d04bd0e3d9e4b6
